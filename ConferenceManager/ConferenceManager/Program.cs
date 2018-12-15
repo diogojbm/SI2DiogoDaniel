46e6e4910ec8792572c5b83a2f3874be6e162743
danielcbmachado <daniel.cbmachado2@gmail.com>
2018-12-15 17:08:25 +0000

    acesso à db feito

--- ConferenceManager/ConferenceManager/Program.cs
+++ ConferenceManager/ConferenceManager/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ConferenceManager
 {
@@ -10,6 +12,25 @@ namespace ConferenceManager
     {
         static void Main(string[] args)
         {
+            try
+            {
+                using (SqlConnection con = new SqlConnection())
+                {
+                    con.ConnectionString = @"Data Source=VLABSIAD;Initial Catalog=ConferênciaAcadémica;Integrated Security=True";
+                    using (SqlCommand cmd = con.CreateCommand())
+                    {
+                        cmd.CommandText = "EXEC ConferênciaAcadémica.AtualizarConferenciaDataLimiteSubmissao '20181121', 'B', 2018";
+                        con.Open();
+                        SqlDataReader dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                            Console.WriteLine(dr[0].ToString() + "-" + dr[1].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO: " + ex.Message);
+            }
         }
     }
 }
